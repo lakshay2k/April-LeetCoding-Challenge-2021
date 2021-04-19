27ee5e0c6e4d8481371ddddbf3b19781cc082318
lakshay2k <lakshaykmsharma@gmail.com>
2021-04-19 10:37:05 +0530

    Global and Local Inversions in C++ - Comments added

--- Week1/day5.cpp
+++ Week1/day5.cpp
@@ -5,12 +5,15 @@ public:
         //considering the deviation from a element is same as the global inversions
         for(int i = 0; i < A.size(); i++)
         {
+
             if(i - A[i] > 1 || i - A[i] < -1)
                 return false;
+
         }
         
         //if deviation comes of more than one, it will make unequal global and local inversions
         //to be equal the difference should be only 1 
+
         return true;
     }
 };
