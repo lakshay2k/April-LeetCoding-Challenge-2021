0d3bbd5edbbd774a7ae02d7571d5fb2156358ef0
lakshay2k <lakshaykmsharma@gmail.com>
2021-04-21 10:53:44 +0530

    Check if string halves are alike in C++ - comments added

--- Week1/day7.cpp
+++ Week1/day7.cpp
@@ -5,13 +5,15 @@ public:
         string vowels = "aeiouAEIOU";
         int mid_value = s.size()/2;
         int res =0 ;
-        for(int i=0,j=mid_value;i<mid_value;i++,j++)
+       
+       for(int i=0,j=mid_value;i<mid_value;i++,j++)
         {
             if(~vowels.find(s[i]))
                 res=res+1;
             if(~vowels.find(s[j]))
                 res=res-1;
         }
+
     return res==0;
     }
 };
