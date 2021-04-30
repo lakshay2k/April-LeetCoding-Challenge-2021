f7c7e35ff11ac5ee6e9164e65bb965ea8e0473ab
lakshay2k <lakshaykmsharma@gmail.com>
2021-04-30 14:53:23 +0530

    Flatten Nested list iterator in C++ - comments added

--- week2/day13.cpp
+++ week2/day13.cpp
@@ -28,26 +28,31 @@ public:
     {
         for(int i =0;i<nestedList.size();i++)
         {
+
             //we keep the integer or we pass the list using recursion again till we get integers
             if(nestedList[i].isInteger())
                 result.push_back(nestedList[i].getInteger());
             else
                 myfunction(nestedList[i].getList());
+
         }
     }
     
     NestedIterator(vector<NestedInteger> &nestedList) {
         myfunction(nestedList);
     }
+
     //here next returns the value of result vector at specified locaton and increment for later use
     int next() {
         return result[ptr++];
         
     }
+
     //here condition is pointer is less than rsultant vector size then it returns true otherwise false
     bool hasNext() {
         return ptr < result.size();
     }
+
 };
 
 /**
