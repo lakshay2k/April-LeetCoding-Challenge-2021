123ba7845329fa4c16f498db5f6bcbe3cbc61482
lakshay2k <lakshaykmsharma@gmail.com>
2021-05-09 10:18:52 +0530

    Power of three in C++

---
+++ Week4/day27.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        //if we get 1 after dividing the number continuously by 3
+        // we can say that it can be expressed in power of 3
+        
+        //to check for zero 
+        if(n>1)
+        {
+        while(n%3 == 0)
+        {
+            n=n/3;
+        }
+        }   
+        return n==1;
+    }
+};
