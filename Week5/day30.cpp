30e10d073f471d4225d22aa3d42f0dcf91463ef8
lakshay2k <lakshaykmsharma@gmail.com>
2021-05-11 18:17:27 +0530

    Powerful integers leetcode problem in C++

---
+++ Week5/day30.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> powerfulIntegers(int x, int y, int bound) {
+        
+        //creating set to remove duplicates 
+        set<int> res;
+        
+        //incrementing bounds by x to have all combinations which are less than bound
+        for(int i=1;i<bound;i=i*x)
+        {
+            for(int j=1;j<bound;j=j*y)
+            {
+                int temp = i + j;;
+                if( temp <= bound)
+                    res.insert(temp);
+                //if the number is 1 then it does not matter, its power will always be 1
+                if(y == 1)
+                    break;
+            }
+            //if the number is 1 then it does not matter, its power will always be 1
+            if(x==1)
+                break;
+        }
+        
+        //converting set into vector and returning
+        vector<int> myres(res.begin(),res.end());
+        return myres;
+    }
+};
